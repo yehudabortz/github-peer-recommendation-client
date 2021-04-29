9cb8c3600078acb66673857408128b0b1e57f7fa
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-29 09:56:17 -0500

    Refactored with default params

--- src/services/fetchCurrentUser.js
+++ src/services/fetchCurrentUser.js
@@ -1,6 +1,12 @@
 import axios from "axios";
+import jwt_decode from "jwt-decode";
+const token = localStorage.getItem("jwt");
+const decoded = jwt_decode(token);
 
-function fetchCurrentUser(url, jwt_token) {
+function fetchCurrentUser(
+  url = `${window.endpoint}/users/${decoded["user_id"]}/account`,
+  jwt_token = token
+) {
   const AuthStr = "Bearer ".concat(jwt_token);
   return axios.get(url, { headers: { Authorization: AuthStr } });
 }
