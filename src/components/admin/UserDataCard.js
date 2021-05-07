f13cfdaab1a5205970ce9b94c745e6d831b5ed0a
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-07 17:23:01 -0500

    Time format

--- src/components/admin/UserDataCard.js
+++ src/components/admin/UserDataCard.js
@@ -2,11 +2,20 @@ import React from "react";
 import "../../css/admin/UserDataCard.css";
 import { connect } from "react-redux";
 import "../../css/Visible.css";
+import Moment from "react-moment";
 function UserDataCard(props) {
   return (
     <div className={"user-data-card-wrap " + props.displayCard}>
       <h1>User card</h1>
-      <h3>{props.selectedUser.name}</h3>
+      <p>{props.selectedUser.name}</p>
+      <p>{props.selectedUser.email}</p>
+      <p>Open To Work: {props.selectedUser.open_to_work ? "✅" : "❌"}</p>
+      <p>
+        Member since{" "}
+        <Moment format="YYYY/MM/DD" parse="YYYY-MM-DD">
+          {props.selectedUser.created_at}
+        </Moment>
+      </p>
     </div>
   );
 }
