a7c98f13bfabec17e19761b5dcfeeae5dc284787
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-13 12:40:05 -0500

    X button for removing message

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import "../css/messages.css";
 import "../css/TextClasses.css";
 import "../css/Visible.css";
+import "../css/Icons.css";
+
+import exitIcon from "../icons/Exit-icon.svg";
 
 export default function Message(props) {
   const [view, setView] = useState("show");
@@ -10,7 +13,13 @@ export default function Message(props) {
     setView("hidden");
   }
   return (
-    <div className={"message-wrap " + view} onClick={handleClick}>
+    <div className={"message-wrap " + view}>
+      <img
+        className={"exit-icon absolute"}
+        src={exitIcon}
+        alt="exit icon"
+        onClick={handleClick}
+      />
       <p>{props.err}</p>
     </div>
   );
