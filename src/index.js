3293ceee58996d7957ec7cf9d99e9ccd23350dab
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-14 12:47:34 -0500

    Remove github client id

--- src/index.js
+++ src/index.js
@@ -10,16 +10,15 @@ import App from "./App.js";
 
 // GLOBAL API ENDPOINT VAR
 // for Prod
-window.endpoint = "https://github-peer-recommendation-api.herokuapp.com";
-window.googleClientId =
-  "998032713164-2i8hgkh3n06l21bptt0e6soji9jtv4jp.apps.googleusercontent.com";
-window.githubClientId = "923c4ab837789f4f569a";
+// window.endpoint = "https://github-peer-recommendation-api.herokuapp.com";
+// window.googleClientId =
+//   "998032713164-2i8hgkh3n06l21bptt0e6soji9jtv4jp.apps.googleusercontent.com";
+// window.githubClientId = "923c4ab837789f4f569a";
 
 // for Dev
-// window.githubClientId = "93ee34c94961f504075d";
-// window.endpoint = "http://localhost:4000";
-// window.googleClientId =
-//   "998032713164-cdesfccd8p7144k50id7alke2c40mlgt.apps.googleusercontent.com";
+window.endpoint = "http://localhost:4000";
+window.googleClientId =
+  "998032713164-cdesfccd8p7144k50id7alke2c40mlgt.apps.googleusercontent.com";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
