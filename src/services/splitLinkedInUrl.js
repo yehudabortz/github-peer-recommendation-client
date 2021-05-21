c531a9c673457f972a866d92669336069072337a
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-20 20:26:56 -0500

    Check to see if string contains 'linkedin' before splitting

--- src/services/splitLinkedInUrl.js
+++ src/services/splitLinkedInUrl.js
@@ -1,5 +1,8 @@
 export function splitLinkedInUrl(url) {
-  if (url) {
+  if (url && url.includes("linkedin.com/in")) {
+    // debugger;
     return url.split("linkedin.com/in/")[1];
+  } else {
+    return url;
   }
 }
