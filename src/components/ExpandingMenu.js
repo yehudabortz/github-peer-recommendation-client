f841fdbe8a32b865186f3990c5e81dbf6e004992
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-25 15:08:34 -0500

    Add animation to icon

--- src/components/ExpandingMenu.js
+++ src/components/ExpandingMenu.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import "../css/ExpandingMenu.css";
+import "../css/Icons.css";
 import exitIcon from "../icons/Exit-icon.svg";
 import chevronIcon from "../icons/chevron-icon.svg";
 const ExpandingMenu = (props) => {
   const [clicked, setClicked] = useState(false);
+  const [rotate, setRotate] = useState("rotate-0");
 
   function handleClick() {
     setClicked(!clicked);
+    if (rotate === "rotate-0") {
+      setRotate("rotate-90");
+    } else {
+      setRotate("rotate-0");
+    }
   }
   return (
     <>
       <img
         onClick={handleClick}
-        className={"icon "}
+        className={`icon ${rotate}`}
         src={chevronIcon}
         alt="chevron icon"
         onClick={(e) => handleClick(e)}
