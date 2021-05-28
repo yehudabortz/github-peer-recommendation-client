3cc24149418c21c98ba4d71987164e6f451a93dd
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-27 19:07:41 -0500

    Change logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,10 @@
-import AnonLogo from "../icons/anon-logo.svg";
+import AnonLogo from "../icons/peer-recommendation-logo.svg";
 const Logo = (props) => {
   return (
     <img
       src={AnonLogo}
       className={`icon logo ${props.size}`}
-      alt="AnonHR Logo"
+      alt="peer recommendation logo"
     />
   );
 };
